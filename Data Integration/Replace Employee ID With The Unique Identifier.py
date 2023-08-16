36230d2787985bb59a8c9a2af31d0d8a7dde99c7
Mikaelow <114190199+Mikaelow@users.noreply.github.com>
2023-08-16 18:51:30 +0200

    Finished Replac Employee With Unique Identifier.py

---
+++ Data Integration/Replace Employee ID With The Unique Identifier.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+def replace_employee_id_with_the_unique_identifier(employees: pd.DataFrame, employee_uni: pd.DataFrame) -> pd.DataFrame:
+    mergedd = pd.merge(employee_uni, employees,  on='id',how = 'right')
+    return mergedd[['unique_id','name']]
+
+data = [[1, 'Alice'], [7, 'Bob'], [11, 'Meir'], [90, 'Winston'], [3, 'Jonathan']]
+Employees = pd.DataFrame(data, columns=['id', 'name']).astype({'id':'int64', 'name':'object'})
+data = [[3, 1], [11, 2], [90, 3]]
+EmployeeUNI = pd.DataFrame(data, columns=['id', 'unique_id']).astype({'id':'int64', 'unique_id':'int64'})
+print(replace_employee_id_with_the_unique_identifier(Employees,EmployeeUNI))
