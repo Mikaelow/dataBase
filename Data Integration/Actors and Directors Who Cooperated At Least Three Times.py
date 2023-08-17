1d274dad92d47fe877976a121548768bec7565da
Mikaelow <114190199+Mikaelow@users.noreply.github.com>
2023-08-17 22:49:34 +0200

    Added task command

--- Data Integration/Actors and Directors Who Cooperated At Least Three Times.py
+++ Data Integration/Actors and Directors Who Cooperated At Least Three Times.py
@@ -1,3 +1,9 @@
+# Write a solution to find all the pairs (actor_id, director_id) where the actor has cooperated with the director at least three times.
+
+# Return the result table in any order.
+
+# The result format is in the following example.
+
 import pandas as pd
 
 def actors_and_directors(actor_director: pd.DataFrame) -> pd.DataFrame:
