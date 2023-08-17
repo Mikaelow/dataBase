6a3f16e92552a342ff56ca461e8f6b9f3bd81f05
Mikaelow <114190199+Mikaelow@users.noreply.github.com>
2023-08-17 22:47:43 +0200

    Added task command

--- Data Integration/Students and Examinations.py
+++ Data Integration/Students and Examinations.py
@@ -1,3 +1,7 @@
+# Write a solution to find the number of times each student attended each exam.
+# Return the result table ordered by student_id and subject_name.
+# The result format is in the following example.
+
 import pandas as pd
 def students_and_examinations(students: pd.DataFrame, subjects: pd.DataFrame, examinations: pd.DataFrame) -> pd.DataFrame:
     merged_table = pd.merge(students, subjects, how = 'cross')
