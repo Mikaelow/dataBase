df423f8dd648d4f71d18970edfd0a744a4d949b9
Mikaelow <114190199+Mikaelow@users.noreply.github.com>
2023-08-20 15:22:55 +0200

    dataBase/Random Stuff/Reformat Department Table.py

---
+++ Random Stuff/Reformat Department Table.py
@@ -0,0 +1,18 @@
+# Reformat the table such that there is a department id column and a revenue column for each month.
+# Return the result table in any order.
+# The result format is in the following example.
+
+import pandas as pd
+
+def reformat_department_table(department : pd.DataFrame):
+    return 0
+
+
+
+data = [[1, 8000, 'Jan'], [2, 9000, 'Jan'], [3, 10000, 'Feb'], [1, 7000, 'Feb'], [1, 6000, 'Mar']]
+Department = pd.DataFrame(data, columns=['id', 'revenue', 'month']).astype(
+    {'id':'Int64', 'revenue':'Int64', 'month':'object'})
+
+
+if(__name__ == '__main__'):
+    print('elo')
