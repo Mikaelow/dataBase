662c8458e28e6f1444f6ceb67ead42d71ba7edf1
Mikaelow <114190199+Mikaelow@users.noreply.github.com>
2023-08-21 18:58:29 +0200

    dataBase/Random Stuff/Combine Two Tables.py

--- Random Stuff/Combine Two Tables.py
+++ Random Stuff/Combine Two Tables.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 def combine_two_tables(person: pd.DataFrame, address: pd.DataFrame) -> pd.DataFrame:
     merged_table = pd.merge(person, address, on = 'personId',how = 'left')
-    # return merged_table[['firstName','lastName','city','state']]
+    #return merged_table[['firstName','lastName','city','state']]
     return person
 
 data = [[1, 'Wang', 'Allen'], [2, 'Alice', 'Bob']]
