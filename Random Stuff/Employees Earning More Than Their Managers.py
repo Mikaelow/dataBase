1296e162ec6dc048b09fc27c9e61e17cca711aab
Mikaelow <114190199+Mikaelow@users.noreply.github.com>
2023-08-21 18:51:48 +0200

    Random Stuff/Employees Earning More Than Their Managers.py

--- Random Stuff/Employees Earning More Than Their Managers.py
+++ Random Stuff/Employees Earning More Than Their Managers.py
@@ -1,10 +1,10 @@
-# Write a solution to find the employees who earn more than their managers.
-# Return the result table in any order.
-# The result format is in the following example.
+#Write a solution to find the employees who earn more than their managers.
+#Return the result table in any order.
+#The result format is in the following example. 
 
 import pandas as pd
 
-def find_employees(employee: pd.DataFrame) -> pd.DataFrame:
+def find_employees(employee: pd.DataFrame)  -> pd.DataFrame:
     merged_table = pd.merge(employee, employee, right_on ='id', left_on ='managerId',how = 'inner')
     higher_salary = merged_table[merged_table['salary_x']>merged_table['salary_y']]
     higher_salary = higher_salary.rename(columns={'name_x':'Employee'})
