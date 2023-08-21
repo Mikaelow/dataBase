660b2ae9573fa832629b00c0fa28a4313c421f40
Mikaelow <114190199+Mikaelow@users.noreply.github.com>
2023-08-21 18:51:58 +0200

    dataBase/Random Stuff/Not Boring Movies.py

--- Random Stuff/Not Boring Movies.py
+++ Random Stuff/Not Boring Movies.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 def not_boring_movies(cinema: pd.DataFrame) -> pd.DataFrame:
     return cinema[(cinema['id']%2 != 0) & (cinema['description'] != 'boring')].sort_values(by='rating', ascending=False)
-
+ 
 
 data = [[1, 'War', 'great 3D', 8.9], [2, 'Science', 'fiction', 8.5], [3, 'irish', 'boring', 6.2], 
         [4, 'Ice song', 'Fantacy', 8.6], [5, 'House card', 'Interesting', 9.1]]
